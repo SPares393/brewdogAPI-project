013444938113a890ae1d9d02d93836186e456a6c
SPares393 <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-11-02 11:47:54 +0000

    mapped over data file to create BeerCard components

--- punk-api-project/src/App.jsx
+++ punk-api-project/src/App.jsx
@@ -4,33 +4,24 @@ import BeerCard from './components/BeerCard/BeerCard';
 import beers from './beers';
 
 const App = () => {
+
+  const createBeerCard = (beer) => {
+    return <BeerCard 
+      name={beer.name}
+      abv={beer.abv}
+      image_url={beer.image_url}
+      tagline={beer.tagline}
+      description={beer.description}
+    />
+  }
+
   return (
     <>
       <section className={styles.NavBar}>
         <NavBar/>
       </section>
       <section className={styles.BeerContainer}>
-        <BeerCard 
-          name={beers[0].name}
-          abv={beers[0].abv}
-          image_url={beers[0].image_url}
-          tagline={beers[0].tagline}
-          description={beers[0].description}
-        />
-                <BeerCard 
-          name={beers[1].name}
-          abv={beers[1].abv}
-          image_url={beers[1].image_url}
-          tagline={beers[1].tagline}
-          description={beers[1].description}
-        />
-                <BeerCard 
-          name={beers[2].name}
-          abv={beers[2].abv}
-          image_url={beers[2].image_url}
-          tagline={beers[2].tagline}
-          description={beers[2].description}
-        />
+        {beers.map(createBeerCard)}
       </section>      
     </>
   );
