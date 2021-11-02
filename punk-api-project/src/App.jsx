e9d463a0cff6dd3da4160628bc3729c7335f7f6a
SPares393 <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-11-02 10:44:55 +0000

    passed props from data file to BeerCard components

--- punk-api-project/src/App.jsx
+++ punk-api-project/src/App.jsx
@@ -1,6 +1,7 @@
 import styles from './App.module.scss';
 import NavBar from './components/NavBar/NavBar';
 import BeerCard from './components/BeerCard/BeerCard';
+import beers from './beers';
 
 const App = () => {
   return (
@@ -9,12 +10,27 @@ const App = () => {
         <NavBar/>
       </section>
       <section className={styles.BeerContainer}>
-        <BeerCard />
-        <BeerCard />
-        <BeerCard />
-        <BeerCard />
-        <BeerCard />
-        <BeerCard />
+        <BeerCard 
+          name={beers[0].name}
+          abv={beers[0].abv}
+          image_url={beers[0].image_url}
+          tagline={beers[0].tagline}
+          description={beers[0].description}
+        />
+                <BeerCard 
+          name={beers[1].name}
+          abv={beers[1].abv}
+          image_url={beers[1].image_url}
+          tagline={beers[1].tagline}
+          description={beers[1].description}
+        />
+                <BeerCard 
+          name={beers[2].name}
+          abv={beers[2].abv}
+          image_url={beers[2].image_url}
+          tagline={beers[2].tagline}
+          description={beers[2].description}
+        />
       </section>
       
     </>
--- punk-api-project/src/components/BeerCard/BeerCard.jsx
+++ punk-api-project/src/components/BeerCard/BeerCard.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import styles from './BeerCard.module.scss';
 
-const BeerCard = () => {
+const BeerCard = (props) => {
     return (
         <div className={styles.BeerCard}>
-            <h2>Beer Name ABV</h2>
+            <h2>{props.name} <span className={styles.BeerCardABV}>({props.abv}%)</span></h2>
             <section className={styles.BeerCardBody}>
-                <img src="https://images.punkapi.com/v2/keg.png" />
+                <img src={props.image_url} alt="Beer Image"/>
                 <section className={styles.BeerCardInfo}>
-                    <h4>Beer tagline info</h4>
-                    <p>
-                        Beer description Beer description Beer description
-                        Beer description Beer description Beer description
-                        Beer description Beer description Beer description
-                    </p>
+                    <h4>{props.tagline}</h4>
+                    <p>{props.description}</p>
                 </section>
             </section>
         </div>
