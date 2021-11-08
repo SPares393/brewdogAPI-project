f31fa16dd4c290cca8e6ff062bb222df233f1c5d
SPares393 <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-11-08 16:19:06 +0000

    added formatName function to BeerCardShort

--- punk-api-project/src/components/BeerCardShort/BeerCardShort.jsx
+++ punk-api-project/src/components/BeerCardShort/BeerCardShort.jsx
@@ -4,6 +4,14 @@ import styles from './BeerCardShort.module.scss';
 const BeerCardShort = (props) => {
     const { name, abv, image_url, tagline, description } = props.beer
 
+    const formatName = (str) => {
+        if (str.length > 20) {
+            return `${str.slice(0, 15)}...`;
+        } else {
+            return str;
+        }
+    }
+
     const formatDescription = (str) => {
         if (str.length > 200) {
             return `${str.slice(0, 200)}...`;
@@ -14,7 +22,7 @@ const BeerCardShort = (props) => {
 
     return (
         <div className={styles.BeerCard}>
-            <h2>{name} <span className={styles.BeerCardABV}>({abv}%)</span></h2>
+            <h2>{formatName(name)} <span className={styles.BeerCardABV}>({abv}%)</span></h2>
             <section className={styles.BeerCardBody}>
                 <img src={image_url} alt="Beer Image"/>
                 <section className={styles.BeerCardDescription}>
