7b38437d3add3ca4ae537381d701783bfb385150
SPares393 <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-11-09 16:37:30 +0000

    github icon added to footer with link to repo

--- punk-api-project/src/components/Footer/Footer.jsx
+++ punk-api-project/src/components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from "./Footer.module.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
         <footer className={styles.Footer}>
-            <p>footer works</p>
+            <a href="https://github.com/SPares393/brewdogAPI-project" target="_blank">            
+                <FontAwesomeIcon icon={faGithub} className={styles.GithubLink}></FontAwesomeIcon>
+            </a>
         </footer>
     )
 }
