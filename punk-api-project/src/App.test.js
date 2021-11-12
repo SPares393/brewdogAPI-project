9764752d8d173f68198b6418e4427c55995a7ba8
SPares393 <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-11-12 17:17:11 +0000

    started unit testing for App and NavBar (not working yet)

--- punk-api-project/src/App.test.js
+++ punk-api-project/src/App.test.js
@@ -1,15 +1,25 @@
 import React from "react";
 import App from "./App";
 import { shallow } from "enzyme";
+import beers from "./beers";
+import BeerCard from "./components/BeerCard/BeerCard";
 
 describe("App tests", () => {
     let component;
+    let mockFn;
 
     beforeEach(() => {
-        component = shallow(<App />);
+        mockFn = jest.fn();
+        component = shallow(<App beers={beers}/>);
     })
 
     it("should render", () => {
         expect(component).toBeTruthy();
     })
+
+    // it("should render the correct number of BeerCard components", () => {
+    //     const numberOfBeers = beers.length;
+    //     const numberOfCards = component.find(<BeerCard />).length;
+    //     expect(numberOfBeers).toEqual(numberOfCards);
+    // })
 })
--- punk-api-project/src/components/NavBar/NavBar.test.js
+++ punk-api-project/src/components/NavBar/NavBar.test.js
@@ -3,13 +3,28 @@ import NavBar from "./NavBar";
 import { shallow } from "enzyme";
 
 describe("NavBar tests", () => {
+    let mockFn;
     let component;
 
     beforeEach(() => {
-        component = shallow(<NavBar />);
+        mockFn = jest.fn();
+        component = shallow(<NavBar onChange={mockFn}/>);
     })
 
     it("should render", () => {
         expect(component).toBeTruthy();
+    })
+
+    it("should call the parent function on change", () => {
+        component.find("input").simulate("click");
+        component.find("input").simulate("input", { target: { value: "User input"}});
+        expect(mockFn).toHaveBeenCalled();
+
+    })
+
+    it("should call the parent function with 'User Input", () => {
+        component.find("input").simulate("click");
+        component.find("input").simulate("input", { target: { value: "User input"}});
+        expect(mockFn).toHaveBeenCalledWith("User Input");
     })
 })
