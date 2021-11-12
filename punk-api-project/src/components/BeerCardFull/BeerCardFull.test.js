3cbcca8dbd9aa018d981116c8d7ec482cedefe35
SPares393 <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-11-12 14:10:42 +0000

    data file linked to BeerCardFull and BeerCardShort test.js files

--- punk-api-project/src/components/BeerCardFull/BeerCardFull.test.js
+++ punk-api-project/src/components/BeerCardFull/BeerCardFull.test.js
@@ -1,12 +1,13 @@
 import React from "react";
 import BeerCardFull from "./BeerCardFull";
 import { shallow } from "enzyme";
+import beers from "../../beers";
 
 describe("BeerCardFull tests", () => {
     let component;
 
     beforeEach(() => {
-        component = shallow(<BeerCardFull />);
+        component = shallow(<BeerCardFull beer={beers[0]}/>);
     })
 
     it("should render", () => {
--- punk-api-project/src/components/BeerCardShort/BeerCardShort.test.js
+++ punk-api-project/src/components/BeerCardShort/BeerCardShort.test.js
@@ -1,12 +1,13 @@
 import React from "react";
 import BeerCardShort from "./BeerCardShort";
 import { shallow } from "enzyme";
+import beers from "../../beers";
 
 describe("BeerCardShort tests", () => {
     let component;
 
     beforeEach(() => {
-        component = shallow(<BeerCardShort />);
+        component = shallow(<BeerCardShort beer={beers[0]}/>);
     })
 
     it("should render", () => {
